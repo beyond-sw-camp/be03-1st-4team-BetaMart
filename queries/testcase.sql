16fa25e0a10a76f2b8b3125dccd3a34ac4aeddfe
GreatJang <148752498+GreatJang@users.noreply.github.com>
2023-12-04 12:04:43 +0900

    Update testcase.sql

--- queries/testcase.sql
+++ queries/testcase.sql
@@ -1,9 +1,9 @@
 -- 고객(Customer) 관련 테스트 케이스
 -- 새로운 고객 생성: 유효한 데이터화 함께 새 고객 레코드 추가.
-INSERT INTO customer(user_id, name, password, tel, age, gender) values('jang4952', 'jangjunhyeok', 'Passw0rd', '010-3748-5573', 25, 'M');|
+INSERT INTO customer(user_id, name, password, tel, age, gender) values('jang4952', '장준혁', 'Passw0rd', '010-3748-5573', 25, 'M');|
 
 -- 중복된 전화번호를 사용하여 고객 추가: 중복 데이터를 사용하여 고객 레코드 추가 시도
-INSERT INTO customer(user_id, name, password, tel, age, gender) values('jang4953', 'jangjunhyeok', 'password', '010-3748-5573', 26, 'W');|
+INSERT INTO customer(user_id, name, password, tel, age, gender) values('jang4953', '장준혁', 'password', '010-3748-5573', 26, 'W');|
 
 -- 상품(Item) 관련 테스트 케이스
     -- 새로운 상품 추가: 유효한 데이터와 함께 새 상품 레코드 추가.
